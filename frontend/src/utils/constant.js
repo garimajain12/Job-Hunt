180f671a9ea01a48b99f9bbd15c379807b9678da
Garima <garimajain120403@gmail.com>
2025-04-07 22:34:36 +0530

    Refactor API endpoint constants to use import.meta.env for environment variables

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,12 @@
-export const USER_API_ENDPOINT = `${process.env.BACKEND_ENDPOINT}/api/v1/user`;
-export const JOB_API_ENDPOINT = `${process.env.BACKEND_ENDPOINT}/api/v1/job`;
-export const APPLICATION_API_ENDPOINT = `${process.env.BACKEND_ENDPOINT}/api/v1/application`;
-export const COMPANY_API_ENDPOINT = `${process.env.BACKEND_ENDPOINT}/api/v1/company`;
+export const USER_API_ENDPOINT = `${
+  import.meta.env.BACKEND_ENDPOINT
+}/api/v1/user`;
+export const JOB_API_ENDPOINT = `${
+  import.meta.env.BACKEND_ENDPOINT
+}/api/v1/job`;
+export const APPLICATION_API_ENDPOINT = `${
+  import.meta.env.BACKEND_ENDPOINT
+}/api/v1/application`;
+export const COMPANY_API_ENDPOINT = `${
+  import.meta.env.BACKEND_ENDPOINT
+}/api/v1/company`;
