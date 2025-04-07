232d6a554a9c18d30e08a39d2b7df8f384cd97f9
Garima <garimajain120403@gmail.com>
2025-04-07 23:51:57 +0530

    Refactor CORS options to use an array of origins and reorder middleware initialization

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connect } from "mongoose";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
@@ -12,15 +11,15 @@ import applicationRoute from "./routes/application.route.js";
 dotenv.config({});
 const app = express();
 
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://getmyjob.vercel.app"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-  origin: process.env.FRONTEND_ENDPOINT,
-  credentials: true,
-};
-console.log(corsOptions);
-app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
